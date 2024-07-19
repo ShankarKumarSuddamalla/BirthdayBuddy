da0c8991a6edee3929ddfeabfd9c90823c8d46b5
shankarkumarsuddamalla <suddamallashankarkumar@gmail.com>
2024-07-19 08:26:42 +0530

    NamesChange

--- src/data.js
+++ src/data.js
@@ -1,33 +1,33 @@
 export default [
     {
       id: 1,
-      name: 'Bertie Yates',
-      age: 29,
-      image: 'https://www.course-api.com/images/people/person-1.jpeg',
+      name: 'Sachin Tendulkar',
+      age: 51,
+      image: 'https://www.livemint.com/lm-img/img/2024/04/24/600x338/PTI04-01-2024-000194B-0_1713933652502_1713933713810.jpg',
     },
     {
       id: 2,
-      name: 'Hester Hogan',
-      age: 32,
-      image: 'https://www.course-api.com/images/people/person-2.jpeg',
+      name: 'Ms Dhoni',
+      age: 42,
+      image: 'https://crictoday.com/wp-content/uploads/2023/03/ms-dhoni-cricket-today-12.webp',
     },
     {
       id: 3,
-      name: 'Larry Little',
-      age: 36,
-      image: 'https://www.course-api.com/images/people/person-3.jpeg',
+      name: 'Suresh Raina',
+      age: 38,
+      image: 'https://timesofindia.indiatimes.com/photo/msid-77817739/77817739.jpg?resizemode=4',
     },
     {
       id: 4,
-      name: 'Sean Walsh',
-      age: 34,
-      image: 'https://www.course-api.com/images/people/person-4.jpeg',
+      name: 'Smriti Mandhana',
+      age: 30,
+      image: 'https://img-cdn.thepublive.com/fit-in/640x430/filters:format(webp)/newsdrum-in/media/media_files/syH7zH056FdP7SSpXssw.jpg',
     },
     {
       id: 5,
-      name: 'Lola Gardner',
-      age: 29,
-      image: 'https://www.course-api.com/images/people/person-5.jpeg',
+      name: 'Virat Kohli',
+      age: 36,
+      image: 'https://wallpapers.com/images/featured/virat-kohli-pictures-yc8dfpcjssp3s4se.jpg',
     },
   ];
   
